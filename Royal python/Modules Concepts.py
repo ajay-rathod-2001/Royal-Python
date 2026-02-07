34c8902dc61cdc0ac778e48a28748a8a4752f88f
ajay-rathod-2001 <ajayrathod20012005@gmail.com>
2026-02-07 23:51:17 +0530

    theory

--- Royal python/Modules Concepts.py
+++ Royal python/Modules Concepts.py
@@ -1,6 +1,12 @@
 # Moduls Concptes :
 
 import os
+
 os.getcwd  # to check the current working Directory
+
+import mymoduls
+
+
+
 
  
--- Royal python/OOPS2 Concepts.py
+++
@@ -1,9 +0,0 @@
-# OOPS CONCEPTS FOUR PILLER :
-# 1) Encapsulation
-# 2) Abstracrion 
-# 3) polymorphism 
-# 4) Inheritence
-
-
-
-
